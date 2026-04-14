8ac159ba0fca0638e9edb337375a2b1b866cf9a2
unknown <jorgealcover@paucasesnovescifp.cat>
2026-04-15 00:34:26 +0200

    Añadido el script de gravedad para los objetos del fondo en el index, falta decorar bien la pagina con ellos

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     actualitzarPodium();
     actualizarBlog();
+
+    createObject(fireZone,-200,150,68.75,"i/Table.png",6);
+    createObject(fireZone,-100, 50,46.87, `i/Stool.png`,4);
+    createObject(fireZone,-300, 50,46.87, `i/Stool.png`,4);
+    startGravity();
+    
 })
+
+let balls=[]
 
 function actualitzarPodium() {
 
@@ -67,3 +75,110 @@ function printUsuaris(usuaris) {
         valor.textContent = usuaris.data[index].name
     })
 }
+
+function createObject(container, x, width, height, color, weight) {
+  const el = document.createElement('img');
+  el.src=color;
+  Object.assign(el.style, {
+    position: 'absolute',
+    width: `${width}px`,
+    height: `${height}px`,
+  });
+
+  container.appendChild(el);
+  const floor = container.offsetHeight/2 - el.offsetHeight;
+  balls.push({
+    el,
+    container,
+    x,
+    y: floor,
+    vy: 0,
+    weight
+  });
+}
+
+let vy, grav, vgrav, bounce, started = true, iterations;
+
+
+function resetGravity() {
+  balls.forEach(b=>{
+    const floor = b.container.offsetHeight/2 - b.el.offsetHeight/2;
+    b.vy = 0;
+    b.grav = 0.1;
+    b.vgrav = 0;
+    b.bounce = 0.9/b.weight;
+    b.el.style.transform = `translate(${b.x}px, ${b.y}px)`;
+  })
+}
+
+function invertGravity() {
+  balls.forEach(b=>{
+  b.grav *= -1;
+  b.vgrav = 0;
+  b.vy = 0;
+  })
+  iterations = 0;
+  startLoop();
+}
+
+function stopLoop() {
+  started = false;
+}
+
+function startLoop() {
+  if (!started) {
+    started = true;
+    requestAnimationFrame(loop);
+  }
+}
+
+function loop() {
+
+  if (!started){
+    return;
+    
+  }
+  iterations++;
+  balls.forEach(b=>{
+    const floor = b.container.offsetHeight/2 - b.el.offsetHeight/2;
+    const ceiling =-floor;
+
+    b.vgrav += b.grav;
+    b.y += b.vy + b.vgrav;
+    b.vy += b.grav;
+    
+  
+    if (b.y > floor) {
+      b.vy = -(b.vy + b.vgrav) * b.bounce;
+      b.y = floor;
+      b.vgrav*=-b.bounce
+    }
+
+    if (b.y < ceiling) {
+      b.y = ceiling;
+      b.vy = -(b.vy + b.vgrav) * b.bounce;
+      b.vgrav*=-b.bounce
+    }
+
+    b.el.style.transform = `translate(${b.x}px, ${b.y}px)`;
+  })
+  requestAnimationFrame(loop);
+  
+  console.log('loop')
+  if (iterations>200){
+    stopLoop();
+  }
+}
+
+
+function startGravity(){
+  resetGravity();
+  loop();
+
+}
+
+window.addEventListener("keydown", function(event){
+    if (event.key == "g"){
+        invertGravity();
+    }
+} )
