afd7f6ccb6c93432cc695c50fd5586ea9c5feadb
AdriaVidal-24 <adriavidal@paucasesnovescifp.cat>
2026-05-10 16:36:00 +0200

    Header scroll
    He arreglat el commit del scroll amb el header

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,44 @@
-import { validateBlog, validateUsers } from "../scripts/utilities.js";
+import { validateBlog, validateUsers } from "./utilities.js";
 
 document.addEventListener("DOMContentLoaded", function() {
 
+    actualitzarPodium();
+    actualizarBlog();
+
+    createObject(Blog,-700,150,68.75,"i/Table.png",6);
+    createObject(Blog,-600, 50,46.87, `i/Stool.png`,4);
+    createObject(Blog,-800, 50,46.87, `i/Stool.png`,4);
+    createObject(Blog,500, 50,50, `i/SmallBox.png`,5);
+    createObject(Blog, 760, 56.25, 71.875, `i/Barrel.png`,7);
+
+    createObject(topPlayers, -700, 100, 100, `i/LargeBox.png`,8);
+    createObject(topPlayers, -626, 50, 50, `i/SmallBox.png`,5);
+    createObject(topPlayers, -786, 75, 75, `i/MediumBox.png`,6);
+    createObject(topPlayers, 700, 50, 46.87, `i/Stool.png`,5);
+    createObject(topPlayers, 600, 150, 68.75, `i/Table.png`,5);
+
+    createObject(media, 200, 50, 50, `i/SmallBox.png`,5);
+    createObject(media, 263, 75, 75, `i/MediumBox.png`,6);
+
+    createObject(lore, -226, 100, 100, `i/LargeBox.png`,8);
+    createObject(lore, -316, 56.25, 71.875, `i/Barrel.png`,7);
+    
+    startGravity();
+    
     const coin = document.querySelector(".coin");
     const body = document.body;
+    coin.classList.add("fly");
+
+    let clicked = true;
+    setTimeout(() => {
+        coin.classList.remove("fly")
+        clicked = false;
+    }, 1760)
+
     const header = document.querySelector("header");
     const gourge = document.getElementsByClassName("gourge")[0];
 
     //Obtenir tema de la pagina i actualitzar-lo
-    let clicked = false;
     let tema = JSON.parse(localStorage.getItem("Tema"));
 
     if (!tema) {
@@ -45,23 +75,15 @@ document.addEventListener("DOMContentLoaded", function() {
             clicked = false;
         }, 1760)
     })
-
-    actualitzarPodium();
-    actualizarBlog();
-
-    setInterval(() => {
-        actualitzarPodium();
-    }, 300000);
 })
+
+let balls=[]
 
 function actualitzarPodium() {
 
     fetch("https://phpstack-1076337-5399863.cloudwaysapps.com/api/classification/uZl9WgoE59y7c3JTN0dyj7KUxkKNP0MpS2NM8msPOZ4eUEtusumqYRHubOGS/3")
         .then(resposta => resposta.json())
-        .then(dades => {
-            validateUsers(dades);
-            printUsuaris(dades);
-        })
+        .then(dades => printUsuaris(dades))
         .catch(error => console.log(error));
 }
 
@@ -69,10 +91,7 @@ function actualizarBlog() {
 
     fetch("https://phpstack-1076337-5399863.cloudwaysapps.com/api/posts/uZl9WgoE59y7c3JTN0dyj7KUxkKNP0MpS2NM8msPOZ4eUEtusumqYRHubOGS")
         .then(resposta => resposta.json())
-        .then(dades => {
-            validateBlog(dades);
-            printPosts(dades);
-        })
+        .then(dades => printPosts(dades))
         .catch(error => console.log(error));
 }
 
@@ -80,31 +99,30 @@ function printPosts(posts) {
 
     const postlist = document.getElementById("post-list");
 
-    for (let i = 0; (i < 3) && (i < posts.data.length); i++) {
-
-        let divText = document.createElement("div");
+    for (let i = 0; (i < 3) && posts.data.length > i; i++) {
+        let p = document.createElement("p");
         let h4 = document.createElement("h4");
         let div = document.createElement("div");
         let article = document.createElement("article");
         let ampli = false;
 
-        divText.classList.add("m-1");
+        p.classList.add("m-1");
         h4.classList.add("m-1");
         article.classList.add("post-item", "row", "j-start", "a-start", "g-1", "p-1", "m-2");
 
-        divText.innerHTML = `${posts.data[i].content.slice(0, 99)}...` 
+        p.innerHTML = `${posts.data[i].content.slice(0, 99)}...` 
 
         h4.textContent = posts.data[i].title;
 
         div.appendChild(h4);
-        div.appendChild(divText);
+        div.appendChild(p);
         article.appendChild(div);
 
         article.addEventListener("click", function() {
             if (!ampli) {
-                divText.innerHTML = posts.data[i].content;
+                p.innerHTML = posts.data[i].content;
             } else {
-                divText.innerHTML = `${posts.data[i].content.slice(0, 99)}...`
+                p.innerHTML = `${posts.data[i].content.slice(0, 99)}...`
             }
             ampli = !ampli;
         })
@@ -123,3 +141,112 @@ function printUsuaris(usuaris) {
         valor.textContent = usuaris.data[index].name
     })
 }
+
+function createObject(container, x, width, height, color, weight) {
+  const el = document.createElement('img');
+  el.src=color;
+  Object.assign(el.style, {
+    position: 'absolute',
+    width: `${width}px`,
+    height: `${height}px`,
+    zIndex: 0,
+  });
+
+  container.appendChild(el);
+  const floor = container.offsetHeight/2 - el.offsetHeight;
+  balls.push({
+    el,
+    container,
+    x,
+    y: floor,
+    vy: 0,
+    weight
+  });
+}
+
+let vy, grav, vgrav, bounce, started = true, iterations;
+
+
+function resetGravity() {
+  balls.forEach(b=>{
+    const floor = b.container.offsetHeight - b.el.offsetHeight;
+    b.vy = 0;
+    b.grav = 0.1;
+    b.vgrav = 0;
+    b.bounce = 0.9/b.weight;
+    b.el.style.transform = `translate(${b.x}px, ${b.y}px)`;
+  })
+}
+
+function invertGravity() {
+  balls.forEach(b=>{
+  b.grav *= -1;
+  b.vgrav = 0;
+  b.vy = 0;
+  })
+  iterations = 0;
+  startLoop();
+}
+
+function stopLoop() {
+  started = false;
+}
+
+function startLoop() {
+  if (!started) {
+    started = true;
+    requestAnimationFrame(loop);
+  }
+}
+
+function loop() {
+
+  if (!started){
+    return;
+    
+  }
+  iterations++;
+  balls.forEach(b=>{
+    const padding = parseFloat(getComputedStyle(b.container).padding);
+    const floor = b.container.offsetHeight - b.el.offsetHeight - padding;
+    const ceiling = -b.el.offsetHeight+ b.el.offsetHeight - padding;
+
+    b.vgrav += b.grav;
+    b.y += b.vy + b.vgrav;
+    b.vy += b.grav;
+    
+  
+    if (b.y > floor) {
+      b.vy = -(b.vy + b.vgrav) * b.bounce;
+      b.y = floor;
+      b.vgrav*=-b.bounce
+    }
+
+    if (b.y < ceiling) {
+      b.y = ceiling;
+      b.vy = -(b.vy + b.vgrav) * b.bounce;
+      b.vgrav*=-b.bounce
+    }
+
+    b.el.style.transform = `translate(${b.x}px, ${b.y}px)`;
+  })
+  requestAnimationFrame(loop);
+  
+  
+  if (iterations>200){
+    //stopLoop();
+  }
+}
+
+
+function startGravity(){
+  resetGravity();
+  loop();
+
+}
+
+window.addEventListener("keydown", function(event){
+    if (event.key == "g"){
+        invertGravity();
+    }
+} )
